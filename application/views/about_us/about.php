4907d9a8db17b8fbf7e43dc14e5c3d60cc55d000
viran pravinda <viran pravinda>
2016-10-27 20:47:00 +0530

    about us page partially created

---
+++ application/views/about_us/about.php
@@ -0,0 +1,74 @@
+<!Doctype html>
+<html>
+<head>
+
+
+
+</head>
+<body>
+
+   <!-- <div>
+        <figure><img class="img-circle" width="304" height="236" src="<?php /*echo base_url(); */?>images/viran.jpg" alt="Cinque Terre"></figure>
+    </div>-->
+
+   <div>
+
+       <h1 style="color: blue;margin-left: 20px;margin-top: 10px">Our Team</h1>
+        <div style="width:350px;margin-top: 20px;margin-left: 10px" class="container">
+            <div class="panel panel-default">
+                <div style="height: 300px" class="panel-body">
+                    <img class="img-circle" width="285" height="230" src="<?php echo base_url(); ?>images/viran.jpg" alt="Cinque Terre">
+                    <h3 style="margin-left: 60px;margin-top: 10px">Viran Pravinda</h3>
+                </div>
+            </div>
+        </div>
+
+       <div style="width:350px;margin-top: -323px;margin-left: 340px" class="container">
+           <div class="panel panel-default">
+               <div style="height: 300px" class="panel-body">
+                   <img class="img-circle" width="285" height="230" src="<?php echo base_url(); ?>images/chameera.jpg" alt="Cinque Terre">
+                   <h3 style="margin-left: 40px;margin-top: 10px">Chameera Kavinda</h3>
+               </div>
+           </div>
+       </div>
+
+       <div style="width:350px;margin-top: -323px;margin-left: 670px" class="container">
+           <div class="panel panel-default">
+               <div style="height: 300px" class="panel-body">
+                   <img class="img-circle" width="285" height="230" src="<?php echo base_url(); ?>images/savidya.jpg" alt="Cinque Terre">
+                   <h3 style="margin-left: 60px;margin-top: 10px">Savidya Sathyani</h3>
+               </div>
+           </div>
+       </div>
+
+       <div style="width:350px;margin-top: -323px;margin-left: 1000px" class="container">
+           <div class="panel panel-default">
+               <div style="height: 300px" class="panel-body">
+                   <img class="img-circle" width="285" height="230" src="<?php echo base_url(); ?>images/isuru.jpg" alt="Cinque Terre">
+                   <h3 style="margin-left: 120px;margin-top: 10px">Isuru</h3>
+               </div>
+           </div>
+       </div>
+
+       <div style="width:350px;margin-top: -323px;margin-left: 1000px" class="container">
+           <div class="panel panel-default">
+               <div style="height: 300px" class="panel-body">
+                   <img class="img-circle" width="285" height="230" src="<?php echo base_url(); ?>images/isuru.jpg" alt="Cinque Terre">
+                   <h3 style="margin-left: 120px;margin-top: 10px">Isuru</h3>
+               </div>
+           </div>
+       </div>
+
+       <div style="width:350px;margin-left: 10px" class="container">
+           <div class="panel panel-default">
+               <div style="height: 300px" class="panel-body">
+                   <img class="img-circle" width="285" height="230" src="<?php echo base_url(); ?>images/nipuni.jpg" alt="Cinque Terre">
+                   <h3 style="margin-top: 10px">Nipuni Lokuhettiarachchi</h3>
+               </div>
+           </div>
+       </div>
+
+   </div>
+
+</body>
+</html>
