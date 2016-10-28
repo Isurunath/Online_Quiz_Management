0a6c3b59e215ce764f002f624fb5b94fbce257c9
Chameera <Chameera>
2016-10-28 20:07:09 +0530

    now it's reqiured a quiz password for download the question paper

--- application/controllers/Paper.php
+++ application/controllers/Paper.php
@@ -11,33 +11,60 @@ class Paper extends CI_Controller
 {
     public function paperlayout()
     {
-
+        $this->load->library('session');
+        if(isset($this->session->userdata['logged_in']))
+        {
         $this->load->model('question_model');
-        $resPaperLayout = $this->question_model->getpaperLayout(1);
-        if($resPaperLayout){
-            $data['single_choice']=$resPaperLayout->single_choice;
-            $data['true_false']=$resPaperLayout->true_false;
-            $data['multiple_choice']=$resPaperLayout->multiple_choice;
+        $resPaperLayout = $this->question_model->getpaperLayout(date("Y-m-d"));
+        if($resPaperLayout) {
+            $data['single_choice'] = $resPaperLayout->single_choice;
+            $data['true_false'] = $resPaperLayout->true_false;
+            $data['multiple_choice'] = $resPaperLayout->multiple_choice;
+            $qpwd = $resPaperLayout->quiz_password;
+            $pwd_frompage = $this->input->post('quiz_pwd');
+
+            if($qpwd == $pwd_frompage) {
+                $this->load->helper('url');
+                $this->load->model('question_model');
+                $data['qestiontype_id'] = $this->question_model->getquestions(1);//result of your query is stored in this ($data['progcategoryid']) variable
+                $data['gettruefalse'] = $this->question_model->getquestions(2);
+                $data['getmultiple'] = $this->question_model->getquestions(3);
+                //$this->load->view('paper/paper', $data);
+
+                //convert to pdf
+                $pdfFilePath = FCPATH . "/downloads/reports/aaaa.pdf";
+                $html = $this->load->view('paper/paper', $data, true); // render the view into HTML
+                $this->load->library('pdf');
+                $pdf = $this->pdf->load();
+                $pdf->useActiveForms = true;
+                $pdf->SetFooter($_SERVER['HTTP_HOST'] . '|{PAGENO}|' . date(DATE_RFC822)); // Add a footer for good measure 😉
+                $pdf->WriteHTML($html); // write the HTML into the PDF
+                $pdf->Output(); // save to file because we can
+                chmod($pdfFilePath, 0777);
+            }
+            else{
+                $data['message'] = 'Invalin Quiz Password';
+                $this->load->helper('url');
+                $this->load->view('header/head1');
+                $this->load->view('paper/downloadPage',$data);
+                $this->load->view('footer/footer1');
+            }
+
+        }
+        else{
+            $this->load->library('session');
+            $this->load->helper('url');
+            $this->load->view('header/head1');
+            $this->load->view('paper/NoPaperToday');
+            $this->load->view('footer/footer1');
         }
 
-        $this->load->helper('url');
-        $this->load->model('question_model');
-        $data['qestiontype_id'] = $this->question_model->getquestions(1);//result of your query is stored in this ($data['progcategoryid']) variable
-        $data['gettruefalse']= $this->question_model->getquestions(2);
-        $data['getmultiple']= $this->question_model->getquestions(3);
-        //$this->load->view('paper/paper', $data);
-
-       //convert to pdf
-        $pdfFilePath = FCPATH."/downloads/reports/aaaa.pdf";
-        $html = $this->load->view('paper/paper', $data, true); // render the view into HTML
-        $this->load->library('pdf');
-        $pdf = $this->pdf->load();
-        $pdf->useActiveForms = true;
-        $pdf->SetFooter($_SERVER['HTTP_HOST'].'|{PAGENO}|'.date(DATE_RFC822)); // Add a footer for good measure 😉
-        $pdf->WriteHTML($html); // write the HTML into the PDF
-        $pdf->Output(); // save to file because we can
-        chmod($pdfFilePath,0777);
-
+        }
+        else{
+            $this->load->helper('url');
+            //$this->load->view('header/head1');
+            $this->load->view('login/new_login');
+        }
 
     }
 
@@ -47,7 +74,7 @@ class Paper extends CI_Controller
           //  $data['$email'] = ($this->session->userdata['logged_in']['email']);
          //   $data['$username'] = ($this->session->userdata['logged_in']['username']);
          //   $data['$type'] = ($this->session->userdata['logged_in']['user_type']);
-        $data1['dd'] =($this->session->userdata['logged_in']['username']);
+      //  $data1['dd'] =($this->session->userdata['logged_in']['username']);
 
             //result of your query is stored in this ($data['progcategoryid']) variable
             //  $this->load->view('paper/paper', $data);
@@ -55,7 +82,7 @@ class Paper extends CI_Controller
             $this->load->view('header/head1');
             //$this->load->view('login/login');
             //$this->load->view('banner/banner1');
-            $this->load->view('paper/downloadPage', $data1);
+            $this->load->view('paper/downloadPage');
             $this->load->view('footer/footer1');
        // }
     }
--- application/views/paper/downloadPage.php
+++ application/views/paper/downloadPage.php
@@ -6,7 +6,20 @@
             <img src="<?php echo base_url(); ?>images/f1.jpg" alt=""/>
         </div>
         <div class="col-md-7 features-right">
-            <a href="<?php echo base_url(); ?>paper/paperlayout"> <h4>Download Question paper</h4></a>
+            <?php
+                if(isset($message))
+                {
+                    echo $message;
+                    echo "<br/>";
+                    echo "<br/>";
+                }
+?>
+<h4>Submit the Quiz Password and Download Question paper</h4>
+<!--            <a href="--><?php //echo base_url(); ?><!--paper/paperlayout"> <h4>Download Question paper</h4></a>-->
+            <form id="qpwd_form" action="<?php echo base_url(); ?>paper/paperlayout" method="post">
+                <input type="password" id="quiz_pwd" name="quiz_pwd" placeholder="Quiz Password">
+                <button type="submit">Submit</button>
+            </form>
         </div>
         <div class="clearfix"></div>
     </div>
